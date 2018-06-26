441fef32fb742803dcd7675f4e6fc337bfbdf7d8
Hugo Abonizio <hugo_abonizio@hotmail.com>
2018-06-26 11:11:56 -0300

    Drop crake dependency

--- make.cr
+++
@@ -1,7 +0,0 @@
-require "crake/global"
-
-task "spec" do
-  sh "crystal run ./spec/test.cr | diff ./spec/expected.txt -"
-  sh "crystal run ./spec/fizzbuzz.cr"
-  sh "env CRYSTAL_TEST=yes crystal run ./spec/fizzbuzz.cr | diff ./spec/fizzbuzz.txt -"
-end
